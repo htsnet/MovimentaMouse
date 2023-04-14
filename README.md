b8fe4ea619e49ea51ac373ca15ec0caa1b30f139
Your NameHamilton <hamilton@hts-net.com.br>
2023-04-14 13:19:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Projeto: Um mecanismo para manter o mouse em funcionamento e previnir que o sistema entre em estado de hibernação  
+Autor: Hamilton Tenório da Silva  
+Date: 14/04/2023  
+
+Veja es projeto em funcionamento em 
+
+------ Instructions ------
+pip install virtualenv  
+python -m venv Ambiente_Virtual  
+Ambiente_Virtual\Scripts\activate.bat
+pip install <packages> (ver a lista em requirements.txt)  
+streamlit run index.py    
---
+++ index.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pyautogui
+import time
+import random
+
+st.title("Movimento Aleatório do Mouse")
+
+# Define as opções de configuração do movimento
+x_range = st.slider("Faixa de Movimento Horizontal", 1, 1000, 500)
+y_range = st.slider("Faixa de Movimento Vertical", 1, 1000, 500)
+interval = st.slider(
+    "Intervalo de Tempo entre os Movimentos (segundos)", 1, 10, 3)
+
+processo = True
+
+# Cria um espaço vazio para mostrar as coordenadas
+coordenadas_texto = st.empty()
+
+# Divide a tela em duas colunas
+col1, col2 = st.columns(2)
+
+# Adiciona um botão na primeira coluna
+with col1:
+    start_movement = st.button("Iniciar Movimento")
+
+# Adiciona um segundo botão na segunda coluna
+with col2:
+    stop_movement = st.button("Parar")
+
+if stop_movement:
+    processo = False
+
+# Inicia o movimento
+if start_movement:
+    while processo:
+        x_offset, y_offset = pyautogui.position()
+        x_offset = abs(random.randint(-x_range, x_range))
+        y_offset = abs(random.randint(-y_range, y_range))
+        # st.write(f"Mouse Movido para ({x_offset}, {y_offset})")
+        # Atualiza o espaço vazio com as novas coordenadas
+        coordenadas_texto.text(f"Movendo para ({x_offset}, {y_offset})")
+        pyautogui.moveTo(x_offset, y_offset, duration=0.5)
+        time.sleep(interval)
